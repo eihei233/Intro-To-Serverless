979514d44701a9e1d79d94fa2c487d17b4a7d99c
eihei233 <jackfirst233@gmail.com>
2022-07-04 12:21:21 -0700

    Update HTTP trigger for let me in check

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -2,9 +2,9 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const password = req.query.password;
-
+    const response = password == "letmein" ? "Access granted." : "Access denied.";
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: response
     };
 }
