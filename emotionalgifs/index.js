c1fb3b22e262826ce6416bc534e1c6763be32bda
eihei233 <jackfirst233@gmail.com>
2022-07-07 16:34:02 -0700

    add get gif

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -23,6 +23,13 @@ async function analyzeImage(img){
     return data; 
 }
 
+async function findGifs(emotion){
+    const sgiphyKey = process.env.GIPHYKEY;
+    const apiResult = await fetch ("https://api.giphy.com/v1/gifs/translate?api_key=" + sgiphyKey + "&s=" + emotion);
+    let data = await apiResult.json();
+    return data.data.url;
+}
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     const body = req.body;
@@ -37,9 +44,10 @@ module.exports = async function (context, req) {
     let emotions = result[0].faceAttributes.emotion;
     let objects = Object.values(emotions);
     const main_emotion = Object.keys(emotions).find(key => emotions[key] === Math.max(...objects));
+    let gif = await findGifs(main_emotion);
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: main_emotion
+        body: gif
     };
     context.done();
     console.log(result);
