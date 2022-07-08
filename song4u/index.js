5ac6324a0c5ba1a67da4d415ca7422859fd9e9af
eihei233 <jackfirst233@gmail.com>
2022-07-07 17:35:43 -0700

    fix url

--- song4u/index.js
+++ song4u/index.js
@@ -3,7 +3,7 @@ const querystring = require('qs');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     const queryObject = querystring.parse(req.body);
-    let url = queryObject.url;
+    let url = queryObject.MediaUrl0;
     context.res = {
         body: url
         };
